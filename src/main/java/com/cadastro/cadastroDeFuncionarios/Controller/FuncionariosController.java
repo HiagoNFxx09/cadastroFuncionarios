5d9c2fe9479cbcaa9cf4c1b7177b3814c96110b3
Hiago <figueiredoh788@gmail.com>
2026-01-17 18:53:02 -0400

    Arrumado o caminho do banco de dados H2, e atribuido o @PostMapping no metodo salavarFuncionario

--- src/main/java/com/cadastro/cadastroDeFuncionarios/Controller/FuncionariosController.java
+++ src/main/java/com/cadastro/cadastroDeFuncionarios/Controller/FuncionariosController.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import com.cadastro.cadastroDeFuncionarios.Business.FuncionariosService;
 import com.cadastro.cadastroDeFuncionarios.Infrastructure.Entity.Funcionarios;
 
 import lombok.RequiredArgsConstructor;
+import lombok.experimental.PackagePrivate;
 
 @RestController
 @RequestMapping ("/funcionarios")
@@ -21,7 +23,7 @@ public class FuncionariosController {
     private final FuncionariosService funcionariosService;
 
 
-    
+    @PostMapping
     public ResponseEntity<Void> salvarFuncionario(@RequestBody Funcionarios funcionarios) {
         funcionariosService.salvarFuncionario(funcionarios);
         return ResponseEntity.ok().build();
