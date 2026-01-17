6f5aa7c256b523aa6b14801a460e7b2dbf0865ac
Hiago <figueiredoh788@gmail.com>
2026-01-17 18:30:33 -0400

    Feito com sucesso o controller

--- src/main/java/com/cadastro/cadastroDeFuncionarios/CadastroDeFuncionariosApplication.java
+++ src/main/java/com/cadastro/cadastroDeFuncionarios/CadastroDeFuncionariosApplication.java
@@ -8,5 +8,7 @@ public class CadastroDeFuncionariosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CadastroDeFuncionariosApplication.class, args);
+
+		System.out.println("\nAplicação de Cadastro de Funcionários iniciada com sucesso!");
 	}
 }
--- src/main/java/com/cadastro/cadastroDeFuncionarios/Controller/FuncionariosController.java
+++ src/main/java/com/cadastro/cadastroDeFuncionarios/Controller/FuncionariosController.java
@@ -1,8 +1,43 @@
 package com.cadastro.cadastroDeFuncionarios.Controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.cadastro.cadastroDeFuncionarios.Business.FuncionariosService;
+import com.cadastro.cadastroDeFuncionarios.Infrastructure.Entity.Funcionarios;
 
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping ("/funcionarios")
+@RequiredArgsConstructor
 public class FuncionariosController {
+
+    private final FuncionariosService funcionariosService;
+
+
+    
+    public ResponseEntity<Void> salvarFuncionario(@RequestBody Funcionarios funcionarios) {
+        funcionariosService.salvarFuncionario(funcionarios);
+        return ResponseEntity.ok().build();
+
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Funcionarios> buscarFuncionariosPorId(@PathVariable Long id) {
+        return ResponseEntity.ok(funcionariosService.buscarFuncionariosPorId(id));
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarFuncionarioPorId(@PathVariable Long id) {
+        funcionariosService.deletarFuncionarioPorId(id);
+        return ResponseEntity.ok().build();
+    }
+
+
 
    
 }
